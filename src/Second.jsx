6b6309e4739838d29300cbbfd476dc6eb57bde2b
Ayushma Sharma <ayushmasharma63@gmail.com>
2020-11-19 22:54:33 +0530

    Update Second.jsx

--- src/Second.jsx
+++ src/Second.jsx
@@ -8,13 +8,14 @@ const Second = (props) => {
                 <div className="row">
                     <div className="col-10 mx-auto">
                       <div className="row">
-                        <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex header-img">
-                            <br></br><br></br><br></br>
-                            <img src={props.imgsrc} className="img-fluid animated" alt="IIITian"/>  
-                            <br></br><br></br><br></br>
+                        <div className="col-md-6 pt-5 pt-lg-0 order-0 order-lg-1 d-flex header-img">
+                            <br></br>
+                            <img src={props.imgsrc} className="img-fluid animated image" alt="IIITian"/>  
+                            <br></br><br></br>
                         </div>
                         <div className="col-lg-6 order-1 order-lg-2 d-flex justify-content-center flex-column">
                             <h1>
+                                <br></br>
                                 <strong className="brand-name"> IIITians Network </strong> {props.heading} 
                             </h1>
                             <h2 className="my-3">
