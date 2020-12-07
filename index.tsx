21b424c368d2025ce980464b7d7a7c6c9a220a27
v-vanisha <68401881+v-vanisha@users.noreply.github.com>
2020-12-07 22:03:56 +0530

    added skills component

---
+++ index.tsx
@@ -0,0 +1,29 @@
+import styles from "./skills.module.scss";
+
+function Skills(props) {
+  return (
+    <div>
+      <h1 className={styles.skills_header}>SKILLS</h1>
+      <div className="container">
+        {/* <div className="row"> */}
+          {props.params.map((skill) => {
+            return (
+              <div className={"col-lg-4 col-md-6 col-sm-12 " + styles.skill}>
+                <img src={skill.image} className={styles.skill_image}></img>
+                <span className={styles.skill_name}>{skill.name}</span>
+                <div className={styles.bar_container}>
+                  <div
+                    className={styles.bar}
+                    style={{ width: skill.level + "%" }}
+                  ></div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
