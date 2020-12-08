b53d792eea902e541cc0603bab2a9597c9e07fb9
v-vanisha <68401881+v-vanisha@users.noreply.github.com>
2020-12-09 00:36:19 +0530

    adding skills component

--- components/skills/index.tsx
+++ components/skills/index.tsx
@@ -3,13 +3,13 @@ import styles from './skills.module.scss';
 function Skills(props) {
   return (
     <div>
-      <h1 className={styles.skills_header}>SKILLS</h1>
+      <h1 className={styles.skills_header}>SKILLS </h1>
       <div className="container">
         <div className="row">
           {props.params.map((skill) => {
             return (
               <div className={'col-lg-4 col-md-6 col-sm-12 ' + styles.skill}>
-                <img src={skill.image} className={styles.skill_image}></img>
+                <img src={skill.images} className={styles.skill_images}></img>
                 <span className={styles.skill_name}>{skill.name}</span>
                 <div className={styles.bar_container}>
                   <div className={styles.bar} style={{ width: skill.level + '%' }}></div>
