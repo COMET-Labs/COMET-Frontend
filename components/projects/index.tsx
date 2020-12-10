74d278495da13fb5600e47b932dc5f5451983211
Vijay Joshi <joshivijay324@gmail.com>
2020-12-10 11:30:38 +0530

    Revert "Added projects section"

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -1,27 +1,9 @@
-import  styles from './projects.module.scss'
+// import  styles from './projects.module.scss'
 
-function Projects(props) {
+function Projects() {
   return (
     <div>
-      <h1 className={styles.projects_header}>WORKS AND PROJECTS</h1>
-      {props.params.map((project)=>{
-        return(
-          <div className="container">
-            <div className={"row "+styles.project}>
-              <img src={project.image} className={"col-lg-4 col-md-6 col-sm-12 "+styles.image}></img>
-              <div className="col-lg-8 col-md-6 col-sm-12">
-                <h3 className={styles.date}>{project.start}-{project.end}</h3>
-                <h2 className={styles.project_name}>{project.name}</h2>
-                <h3 className={styles.project_matter}>{project.matter}</h3>
-                <div className={styles.all_icons}>
-                  <a href={project.link}><span className={"iconify "+styles.icon} data-icon="mdi:web" color="black" data-inline="false"></span></a>
-                  <a href={project.github}><span className={"iconify "+styles.icon} data-icon="entypo:code" color="black" data-inline="false"></span></a>
-              </div>
-              </div>
-            </div>
-          </div>
-        )
-      })}
+      <h1>This is a Works and Projects section.</h1>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <Education params={PortFolioData["education"]}/>
       <Skills params={PortFolioData["skills"]}/>
       <WorkExperience params={PortFolioData["work-experience"]}/>
-      <Projects params={PortFolioData["projects"]}/>
+      <Projects />
     </Layout>
   );
 }
