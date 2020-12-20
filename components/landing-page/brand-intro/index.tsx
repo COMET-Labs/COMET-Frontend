8d1f0ae2eabb0419fbc265c2ca27f5d911cb5f11
vijayjoshi16 <joshivijay324@gmail.com>
2020-12-20 22:45:59 +0530

    Update file structure to add landing page

---
+++ components/landing-page/brand-intro/index.tsx
@@ -0,0 +1,11 @@
+import styles from './brand-intro.module.scss';
+
+function BrandIntro(){
+    return(
+        <div className={styles.brand_intro}>
+            This is the brand intro component.
+        </div>
+    )
+}
+
+export default BrandIntro;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,11 @@
-import MainIntro from '../components/main-intro';
-import AboutMe from '../components/aboutme';
-import Education from '../components/education';
-import Skills from '../components/skills';
-import WorkExperience from '../components/work-experience';
-import Projects from '../components/projects';
-import Layout from '../components/layouts/default';
+import BrandIntro from '../components/landing-page/brand-intro';
+import Features from '../components/landing-page/features';
 
-import PortFolioData from '../PortFolioData';
-import NavBar from '../components/navbar';
-
-
-export default function Home() {
-  return (
-    <Layout>
-      
-      <MainIntro params={PortFolioData["intro"]}/>
-      <AboutMe />
-      <Education params={PortFolioData["education"]}/>
-      <Skills params={PortFolioData["skills"]}/>
-      <WorkExperience params={PortFolioData["work-experience"]}/>
-      <Projects params={PortFolioData["projects"]}/>
-    </Layout>
+export default function Home(){
+  return(
+    <div>
+      <BrandIntro />
+      <Features />
+    </div>
   );
 }
