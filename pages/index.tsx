9cbb8e38c22c2080d6883aeb46c18e3069cff6df
Tushar Jain <tusharjain0022@gmail.com>
2021-01-06 20:19:43 +0530

    landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
+import React, { useRef, useEffect, useState, Suspense } from 'react';
 import BrandIntro from '../components/landing-page/brand-intro';
 import Features from '../components/landing-page/features';
+import { Canvas } from 'react-three-fiber';
+import { Stars } from 'drei';
+import state from '../components/landing-page/state';
 
-export default function Home(){
-  return(
-    <div>
-      <BrandIntro />
-      <Features />
-    </div>
+export default function Home() {
+  const domContent = useRef();
+  const scrollArea = useRef();
+  const onScroll = (e) => (state.top.current = e.target.scrollTop);
+  useEffect(() => void onScroll({ target: scrollArea.current }), []);
+  return (
+    <>
+      <Canvas colorManagement camera={{ position: [0, 0, 120], fov: 70 }}>
+        <Suspense fallback={null}>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Stars />
+          <BrandIntro domContent={domContent} />
+          <Features domContent={domContent} />
+        </Suspense>
+      </Canvas>
+      <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+        <div style={{ position: 'sticky', top: 0 }} ref={domContent} />
+        <div style={{ height: `${state.sections * 100}vh` }} />
+      </div>
+    </>
   );
 }
